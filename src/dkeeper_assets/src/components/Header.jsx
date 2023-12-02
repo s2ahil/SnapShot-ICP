035129f64734d07f32467b2b07c280cb34a7bffc
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-12-02 10:44:16 +0530

    Add files via upload

---
+++ src/dkeeper_assets/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+function Header() {
+  return (
+    <header style={{backgroundColor:"#A435F0"}}>
+      <h1 >
+        <HighlightIcon />
+        SnapShot-ICP
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
